c7c1429254cfefa412188abd38b9df5584750969
Jerry <jerry.ho.1983@gmail.com>
2017-08-05 14:49:45 +0700

    init angular/cli with boostrap (AlertModule)

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{AlertModule} from 'ng2-bootstrap/ng2-bootstrap';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AlertModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
